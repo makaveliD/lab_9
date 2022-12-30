89bfce567d002b5bdfcde0f03df12c76c9c1c7b8
Vova Zabalotny <vovazabalotny@gmail.com>
2022-12-30 18:37:34 +0200

    add basic functions

---
+++ lab_9.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void main()
+{
+	string sentance;
+	string delimiter = " ";
+	string token;
+	vector<string> sentanceParts;
+	int length;
+	int position;
+	cout << "Enter string: "<<endl;
+	getline(cin, sentance);
+	
+	cout << endl << "Enter length: "<<endl;
+	cin >> length;
+	cout << sentance << ":" << length;
+	position = sentance.find(delimiter);
+	while (position > 0) {
+		token = sentance.substr(0, position);
+		sentanceParts.push_back(token);
+		sentance.erase(0, position + delimiter.length());
+		position = sentance.find(delimiter);
+	}
+	for (int i = 0; i < sentanceParts.size(); i++) {
+		cout << sentanceParts[i] << endl;
+	}
+	
+}
